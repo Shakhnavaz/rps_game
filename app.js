c8c13b480fffb228a3a907a024d23cd8bfafb59f
Shakhnavaz <shakhnavaz@gmail.com>
2022-11-19 11:22:37 +0300

    random engine (do not work)

---
+++ app.js
@@ -0,0 +1,25 @@
+function getComputerChoice(userInput) {
+    // if (userInput == 'rock' || userInput == 'paper' || userInput == 'scissors') {
+    //     console.log(userInput)
+    // } else {
+    //     console.log('please input: rock, paper or scissors') 
+    // } 
+    let rps 
+    let mathResult = Math.floor(Math.random() * 3);
+    switch (mathResult == 0 )
+    // if (mathResult == 0) {
+    //     rps = 'rock'
+    // } else if (mathResult == 1) { 
+    //     rps = 'paper'
+    // } else if (mathResult == 2) {
+    //     rps = 'scissors'
+    // } 
+    
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice(playerSelection);
